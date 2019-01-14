daeab04eda7600cd43565c218b78359f9ff0f36e
valerian18 <valerian.gonnot@gmail.com>
2019-01-14 15:23:24 +0100

    Update setup_cron.sh
    Add person detectionroutine ton crontab

--- setup_cron.sh
+++ setup_cron.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+#write out current crontab
+crontab -l > cron_file
+#echo new cron into cron file
+echo "*/5 08-18 * * 1-5 ./$FIYELI_CORE_ROUTINE" >> cron_file
+#install new cron file
+crontab cron_file
+rm cron_file
