dc177963c53010647ea1b1db1350a335bfdf735a
valerian18 <valerian.gonnot@gmail.com>
2019-01-14 15:24:29 +0100

    Update setup_cron.sh
    Syntax fix

--- setup_cron.sh
+++ setup_cron.sh
@@ -2,7 +2,7 @@
 #write out current crontab
 crontab -l > cron_file
 #echo new cron into cron file
-echo "*/5 08-18 * * 1-5 ./$FIYELI_CORE_ROUTINE" >> cron_file
+echo "*/5 08-18 * * 1-5 $FIYELI_CORE_ROUTINE" >> cron_file
 #install new cron file
 crontab cron_file
 rm cron_file
