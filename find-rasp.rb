db80fcbb2a4471f8eb7db64937eaea87d027a704
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2019-01-14 10:07:23 +0100

    find ip ok with hooray

---
+++ find-rasp.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'hooray'
+
+def find_raspberries
+  seek = Hooray::Seek.new
+  raspberries = seek.nodes.select do |element|
+    # mac adress of raspberry
+    element.mac[0..7].upcase.tr(':', '') == 'B827EB'
+  end
+  raspberries
+end
+
+def scan_network_and_test_ssh(hostname)
+  20.times do
+    raspberries = find_raspberries
+    raspberries.each do |raspberry|
+      real_hostname = `sshpass -p raspberry \
+      ssh -oStrictHostKeyChecking=no pi@#{raspberry.ip.to_s} hostname`
+    p real_hostname
+      return raspberry.ip if hostname.chomp == real_hostname.chomp
+    end
+    sleep(5)
+  end
+  nil
+end
+
+def find_rasp(hostname)
+  res = scan_network_and_test_ssh(hostname)
+  return res.to_s if res.class.to_s == 'IPAddr'
+
+  'NOT FOUND'
+end
+
--- sd-card.rb
+++ sd-card.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
 require 'tty-prompt'
 require 'ruby-progressbar'
 require 'mkmf'
@@ -9,21 +10,16 @@ require 'fileutils'
 require 'zip'
 require 'faker'
 
-# TODO: understand if we have to use loop0p1 or mm**1
-# or we have to write before to dd image !
-# we have to forgot loop0p1 ....
-# dd must be last step
-
 TMP_RASPBIAN = '/tmp/raspbian_lite_latest'.freeze
 TMP_RASPBIAN_IMG = '/tmp/raspbian-stretch-lite.img'.freeze
 TMP_BUILDING_IMG = '/tmp/fiyeli.img'.freeze
 TMP_MOUNT_ENDPOINT = '/tmp/raspmount'.freeze
 TMP_WPA_SUPPLICANT = '/tmp/wpa-supplicant'.freeze
 
-DEV_MAPPER = nil
+PROMPT = TTY::Prompt.new
 
 def exec_with_sudo(cmd, message)
-  res = ""
+  res = ''
   if ENV['USER'] != 'root'
     puts message
     puts "CMD: sudo #{cmd}"
@@ -32,19 +28,11 @@ def exec_with_sudo(cmd, message)
   else
     res = `#{cmd}`
   end
-  return res
+  res
 end
 
 def shut_down
-  puts 'sync'
-  `sync`
-  puts "\n remove kpartx partition"
-  cmd_kpartx = "kpartx -v -d #{TMP_BUILDING_IMG}"
-  exec_with_sudo(cmd_kpartx, 'need root access to use kpartx')
-  puts "\n umount gracefully..."
-  cmd_umount = "umount #{TMP_MOUNT_ENDPOINT}"
-  exec_with_sudo(cmd_umount,
-                 "need root access to umount sd card on #{TMP_MOUNT_ENDPOINT}")
+  # nothing for the moment
 end
 
 def mount_part(disk)
@@ -56,114 +44,230 @@ def mount_part(disk)
                  "need root access to mount sd card on #{TMP_MOUNT_ENDPOINT}")
 end
 
-prompt = TTY::Prompt.new
-
-required_bin = %w[tail kpartx]
-required_bin.each do |bin|
-  unless find_executable(bin)
-    STDERR.puts("ABORTED! You have to install #{bin}")
-    exit(false)
+def check_prerequisite
+  required_bin = %w[tail kpartx nmap sshpass]
+  required_bin.each do |bin|
+    unless find_executable(bin)
+      STDERR.puts("ABORTED! You have to install #{bin}")
+      exit(false)
+    end
   end
 end
 
-exlude_dist_contains = ['nvme']
-disk = `lsblk --noheadings --raw -o NAME,MOUNTPOINT \
- | awk '$1~/[[:digit:]]/ && $2 == ""'`
-       .lines.map { |e| "/dev/#{e.chomp}" }
-       .delete_if do |element|
-  exlude_dist_contains.any? { |e| element.include?(e) }
-end
-sd = prompt.select('Choose your disk?', disk).split[0].inspect
+def find_disk
+  exlude_dist_contains = ['nvme']
+  disk = `lsblk --noheadings --raw -o NAME,MOUNTPOINT \
+   | awk '$1~/[[:digit:]]/ && $2 == ""'`
+         .lines.map { |e| "/dev/#{e.chomp}" }
+         .delete_if do |element|
+    exlude_dist_contains.any? { |e| element.include?(e) }
+  end
+  sd = PROMPT.select('Choose your disk?', disk).split[0].inspect
 
-if File.file?(TMP_RASPBIAN)
-  puts "raspbian is already download here #{TMP_RASPBIAN}"
-else
-  puts 'download latest raspbian'
+  sd
+end
+
+def check_before_downlaod
+  if File.file?(TMP_RASPBIAN)
+    puts "raspbian is already download here #{TMP_RASPBIAN}"
+    false
+  else
+    puts 'download latest raspbian'
+    true
+  end
+end
+
+def download_latest_zip
+  return unless check_before_downlaod
+
   pbar = nil
   download = open('https://downloads.raspberrypi.org/raspbian_lite_latest', \
                   allow_redirections: :all, \
                   content_length_proc: lambda { |t|
                     pbar = ProgressBar.create(total: t) if t && t > 0
-                  },
-
-                  progress_proc: lambda { |s|
-                    pbar.progress = s if pbar
-                  })
+                  }, progress_proc: lambda do |s|
+                                      pbar.progress = s if pbar
+                                    end)
   IO.copy_stream(download, TMP_RASPBIAN)
 end
 
-if File.file?(TMP_RASPBIAN_IMG)
-  puts "raspbian already unziped here #{TMP_RASPBIAN_IMG}"
-else
-  puts 'unzip raspian'
-  Zip::File.open(TMP_RASPBIAN) do |zip_file|
-    # Find specific entry
-    entry = zip_file.glob('*.img').first
-    # Extract to file/directory/symlink
-    puts "Extracting #{entry.name}"
-    entry.extract(TMP_RASPBIAN_IMG)
+def unzip_img
+  if File.file?(TMP_RASPBIAN_IMG)
+    puts "raspbian already unziped here #{TMP_RASPBIAN_IMG}"
+  else
+    puts 'unzip raspian'
+    Zip::File.open(TMP_RASPBIAN) do |zip_file|
+      # Find specific entry
+      entry = zip_file.glob('*.img').first
+      # Extract to file/directory/symlink
+      puts "Extracting #{entry.name}"
+      entry.extract(TMP_RASPBIAN_IMG)
+    end
   end
 end
 
-# copy the raw raspbian image to building image
-`cp #{TMP_RASPBIAN_IMG} #{TMP_BUILDING_IMG}`
-# use kpartx / kpartx create simlink
-cmd_kpartx = "sudo kpartx -v -a #{TMP_BUILDING_IMG}"
-# should be like loop0p1,loop0p2
-DEV_MAPPER = exec_with_sudo(cmd_kpartx, 'need root access to use kpartx')
-             .scan(/(loop.*)\s\(/)
-puts DEV_MAPPER
+def download_and_extract
+  download_latest_zip
+  unzip_img
+end
 
-puts('set up boot partition')
-mount_part("/dev/mapper/#{DEV_MAPPER[0].first}")
+def prepare_img
+  # copy the raw raspbian image to building image
+  `cp #{TMP_RASPBIAN_IMG} #{TMP_BUILDING_IMG}`
+  # use kpartx / kpartx create simlink
+  cmd_kpartx = "sudo kpartx -v -a #{TMP_BUILDING_IMG}"
+  # should be like loop0p1,loop0p2
+  dev_mapper = exec_with_sudo(cmd_kpartx, 'need root access to use kpartx')
+               .scan(/(loop.*)\s\(/)
+  puts dev_mapper
 
-# Trap ^C
-Signal.trap('INT') do
+  puts('set up boot partition')
+  mount_part("/dev/mapper/#{dev_mapper[0].first}")
+  dev_mapper
+end
+
+def set_trap
+  # Trap ^C
+  Signal.trap('INT') do
+    shut_down
+    exit
+  end
+
+  # Trap `Kill `
+  Signal.trap('TERM') do
+    shut_down
+    exit
+  end
+end
+
+def enable_ssh
+  cmd_touch = "echo 'ssh file at boot' | sudo tee #{TMP_MOUNT_ENDPOINT}/ssh"
+  exec_with_sudo(cmd_touch, 'need root to touch ssh at /boot')
+end
+
+def create_wpa_supplicant
+  ssid = PROMPT.ask('Give me your SSID ?')
+  passwd = PROMPT.ask('Give me your password ?')
+  File.open(TMP_WPA_SUPPLICANT, 'w') do |file|
+    file.write("ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+  network={
+      ssid=\"#{ssid}\"
+      psk=\"#{passwd}\"
+      key_mgmt=WPA-PSK
+  }")
+  end
+end
+
+def enable_wpa
+  create_wpa_supplicant
+  cmd_create_wpa_supplicant =
+    "cp #{TMP_WPA_SUPPLICANT} #{TMP_MOUNT_ENDPOINT}/wpa_supplicant.conf"
+  exec_with_sudo(cmd_create_wpa_supplicant,
+                 'need root to mv wpa_supplicant.conf at /boot')
+end
+
+def setup_rasp_boot
+  enable_ssh
+  enable_wpa
+end
+
+def umount_part1_img
+  # unmounted boot partition
+  puts 'sync'
+  `sync`
+  cmd_umount = "umount #{TMP_MOUNT_ENDPOINT}"
+  exec_with_sudo(cmd_umount,
+                 "need root access to umount sd card on #{TMP_MOUNT_ENDPOINT}")
+end
+
+def build_part2_img(dev_mapper)
+  puts('end setting boot partition')
+  mount_part("/dev/mapper/#{dev_mapper[1].first}")
+
+  hostname = "fiyeli-#{Faker::Science.scientist.gsub!(/\s/, '-')}"
+  cmd_hostname = "echo #{hostname} "\
+                 " | sudo tee #{TMP_MOUNT_ENDPOINT}/etc/hostname"
+  exec_with_sudo(cmd_hostname,
+                 "need root access to umount sd card on #{TMP_MOUNT_ENDPOINT}")
+  hostname
+end
+
+def umount_part2_img_and_remove_mapper
+  puts 'sync'
+  `sync`
+  puts "\n umount gracefully..."
+  cmd_umount = "umount #{TMP_MOUNT_ENDPOINT}"
+  exec_with_sudo(cmd_umount,
+                 "need root access to umount sd card on #{TMP_MOUNT_ENDPOINT}")
+  puts "\n remove kpartx partition"
+  cmd_kpartx = "kpartx -v -d #{TMP_BUILDING_IMG}"
+  exec_with_sudo(cmd_kpartx, 'need root access to use kpartx')
+end
+
+def dd_image_on_sd(sd_dev)
+  cmd_dd = "dd if=#{TMP_BUILDING_IMG} of=#{sd_dev} \
+  bs=4M conv=fsync status=progress"
+  exec_with_sudo(cmd_dd, 'need root access to dd image on sd card')
+
+  puts 'remove and put the sd card in raspberry'
+end
+
+def wait_and_scan(hostname)
+  PROMPT.keypress('Press key for run scan')
+  puts 'during 90 seconds the script scan network waiting the raspberry'
+
+  require_relative 'find-rasp'
+
+  ip = find_rasp(hostname)
+  if ip != 'NOT FOUND'
+    puts "your raspberry IP is : #{ip}"
+  else
+    puts 'timeout your raspberry was not found'
+  end
+end
+
+def ensure_context
+  check_prerequisite
+  set_trap
+end
+
+def part2(dev_mapper)
+  hostname = build_part2_img(dev_mapper)
+
+  umount_part2_img_and_remove_mapper
+  hostname
+end
+
+def main
+  ensure_context
+
+  sd = find_disk
+
+  download_and_extract
+
+  dev_mapper = prepare_img
+
+  setup_rasp_boot
+
+  umount_part1_img
+
+  hostname = part2(dev_mapper)
+
+  dd_image_on_sd(sd)
+
+  wait_and_scan(hostname)
+
   shut_down
-  exit
 end
 
-# Trap `Kill `
-Signal.trap('TERM') do
-  shut_down
-  exit
+options = {}
+OptionParser.new do |opt|
+  opt.on('--scan HOSTNAME') { |o| options[:scan] = o }
+end.parse!
+
+if options.include? :scan
+  wait_and_scan options[:scan]
+else
+  main
 end
 
-cmd_touch = "echo 'make ssh file at boot' | sudo tee #{TMP_MOUNT_ENDPOINT}/ssh"
-exec_with_sudo(cmd_touch, 'need root to touch ssh at /boot')
-
-ssid = prompt.ask('Give me your SSID ?')
-passwd = prompt.ask('Give me your password ?')
-File.open(TMP_WPA_SUPPLICANT, 'w') do |file|
-  file.write("ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-network={
-    ssid=\"#{ssid}\"
-    psk=\"#{passwd}\"
-    key_mgmt=WPA-PSK
-}")
-end
-cmd_create_wpa_supplicant =
-  "cp #{TMP_WPA_SUPPLICANT} #{TMP_MOUNT_ENDPOINT}/wpa_supplicant.conf"
-exec_with_sudo(cmd_create_wpa_supplicant,
-               'need root to mv wpa_supplicant.conf at /boot')
-
-# unmounted boot partition
-puts 'sync'
-`sync`
-cmd_umount = "umount #{TMP_MOUNT_ENDPOINT}"
-exec_with_sudo(cmd_umount,
-               "need root access to umount sd card on #{TMP_MOUNT_ENDPOINT}")
-
-puts('end setting boot partition')
-mount_part("/dev/mapper/#{DEV_MAPPER[1].first}")
-
-cmd_hostname = "echo  fiyeli-#{Faker::Science.scientist.gsub!(/\s/, '-')} "\
-             " | sudo tee #{TMP_MOUNT_ENDPOINT}/etc/hostname"
-exec_with_sudo(cmd_hostname,
-               "need root access to umount sd card on #{TMP_MOUNT_ENDPOINT}")
-
-cmd_dd = "dd if=#{TMP_BUILDING_IMG} of=#{sd} \
-bs=4M conv=fsync status=progress"
-exec_with_sudo(cmd_dd, 'need root access to dd image on sd card')
-
-shut_down
