013b5337f415b4ee775428bb5fbf9b581490ad20
ThibaultRu <44357199+ThibaultRu@users.noreply.github.com>
2019-01-20 19:11:59 +0100

    Delete unused setup_cron.sh

--- setup_cron.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Write out current crontab
-crontab -l > cron_file
-# Echo new cron into cron file
-echo "*/5 08-18 * * 1-5 $FIYELI_CORE_ROUTINE" >> cron_file
-        # Install new cron file
-        crontab cron_file
-        rm cron_file
